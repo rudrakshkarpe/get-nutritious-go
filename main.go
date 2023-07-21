cb211e102c130180cf66f13413198a6fc1ff2ce9
Rudraksh Karpe <rudraksh.karpe@gmail.com>
2023-07-22 00:45:18 +0530

    Pass parameters to methods as nutrition values

--- main.go
+++ main.go
@@ -6,15 +6,15 @@ import (
 
 func main() {
 	ns := GetNutritionalScore(NutritionalData{
-		Energy:              EnergyFromKcal(),
-		Sugars:              SugarGram(),
-		SaturatedFattyAcids: SaturatedFattyAcids(),
-		Sodium:              SodiumMilligram(),
-		Friuits:             FruitsPercet(),
-		Fiber:               FiberGram(),
-		Proteins:            ProteinGram(),
+		Energy:              EnergyFromKcal(0),
+		Sugars:              SugarGram(10),
+		SaturatedFattyAcids: SaturatedFattyAcidsGram(2),
+		Sodium:              SodiumMilligram(500),
+		Fruits:              FruitsPercet(60),
+		Fiber:               FiberGram(4),
+		Protein:             ProteinGram(2),
 	}, Food)
 
 	fmt.Printf("Nutritional Score: %d\n", ns.Value)
-
+	fmt.Printf("NutriScore: %s\n", ns.GetNutriScore())
 }
